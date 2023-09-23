acfbdd3a2c4c6a79009c4748f96d3e9f51f9b1ae
Wade Cappa <wade.cappa@msn.com>
2023-09-23 12:55:05 -0700

    updated run scripts

--- slurm_scripts/perlmutter_lazy_lazy.sh
+++ slurm_scripts/perlmutter_lazy_lazy.sh
@@ -4,11 +4,11 @@
 #SBATCH -C cpu
 #SBATCH -t 00:10:00
 #SBATCH -q regular
-#SBATCH -N 64
+#SBATCH -N 256
 #SBATCH --ntasks-per-node=1
-#SBATCH -J m64_l1_bMAX_leveled_orkut_small_IC
-#SBATCH -o /global/homes/w/wadecap/results/jobs/testing_leveled/orkut_small/m64_l1_bMAX_leveled_orkut_small_IC.o
-#SBATCH -e /global/homes/w/wadecap/results/jobs/testing_leveled/orkut_small/m64_l1_bMAX_leveled_orkut_small_IC.e
+#SBATCH -J m256_l1_bMAX_leveled_orkut_small_IC
+#SBATCH -o /global/homes/w/wadecap/results/jobs/testing_leveled/orkut_small/m256_l1_bMAX_leveled_orkut_small_IC.o
+#SBATCH -e /global/homes/w/wadecap/results/jobs/testing_leveled/orkut_small/m256_l1_bMAX_leveled_orkut_small_IC.e
 
 # module use /global/common/software/m3169/perlmutter/modulefiles
 module use /global/common/software/m3169/perlmutter/modulefiles
@@ -27,4 +27,4 @@ module load cray-libsci
 #module load openmpi
 #module load cudatoolkit/11.0
 
-srun -n 64 ./build/release/tools/mpi-randgreedi -i /global/cfs/cdirs/m1641/network-data/Binaries/orkut_small_IC_binary.txt -w -k 100 -p -d IC -e 0.13 -o /global/homes/w/wadecap/results/jobs/testing_leveled/orkut_small/m64_l1_bMAX_leveled_orkut_small_IC.json --run-streaming=false --reload-binary -u
+srun -n 256 ./build/release/tools/mpi-randgreedi -i /global/cfs/cdirs/m1641/network-data/Binaries/orkut_small_IC_binary.txt -w -k 100 -p -d IC -e 0.13 -o /global/homes/w/wadecap/results/jobs/testing_leveled/orkut_small/m256_l1_bMAX_leveled_orkut_small_IC.json --run-streaming=false --reload-binary -u
